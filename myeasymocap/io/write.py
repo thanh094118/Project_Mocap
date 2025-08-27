006dcb42cf95d528f23d684a8f94ca973b7f5669
thanh094118 <buithanh30082004@gmail.com>
2025-08-27 11:36:03 +0700

    add class into render and write...

--- myeasymocap/io/write.py
+++ myeasymocap/io/write.py
@@ -4,67 +4,6 @@ from easymocap.annotator.file_utils import save_annot
 from os.path import join
 from tqdm import tqdm
 
-class Write:
-    def __init__(self, output='/tmp', name='keypoints3d') -> None:
-        self.output = output
-        self.name = name
-    
-    def __call__(self, keypoints3d):
-        for nf in tqdm(range(keypoints3d.shape[0]), desc='writing to {}/{}'.format(self.output, self.name)):
-            res = [{
-                'id': 0,
-                'keypoints3d': keypoints3d[nf]
-            }]
-            dumpname = join(self.output, self.name, '{:06d}.json'.format(nf))
-            write_keypoints3d(dumpname, res)
-        return {}
-
-class WriteAll:
-    def __init__(self, name, output='/tmp') -> None:
-        self.output = output
-        self.name = name
-    
-    def __call__(self, results, meta):
-        for nf in tqdm(range(len(results)), desc='writing to {}/{}'.format(self.output, self.name)):
-            res = [{'id': r['id'], 'keypoints3d': r['keypoints3d']} for r in results[nf]]
-            res.sort(key=lambda x: x['id'])
-            imgnames = meta['imgnames'][nf]
-            if len(imgnames) > 0:
-                name = os.path.basename(imgnames[0])
-                name = name.replace('.jpg', '')
-            else:
-                name = '{:06f}'.format(nf)
-            dumpname = join(self.output, self.name, '{}.json'.format(name))
-            write_keypoints3d(dumpname, res)
-
-class Write2D:
-    def __init__(self, name, output='/tmp') -> None:
-        self.output = output
-        self.name = name
-    
-    def __call__(self, results, meta):
-        for nf in tqdm(range(len(results)), desc='writing to {}/{}'.format(self.output, self.name)):
-            subs = meta['subs'][nf]
-            result = results[nf]
-            annots_all = {sub: [] for sub in subs}
-            for res in result:
-                for nv, v in enumerate(res['views']):
-                    annots_all[subs[v]].append({
-                        'personID': res['id'],
-                        'bbox': res['bbox'][nv],
-                        'keypoints': res['keypoints2d'][nv],
-                    })
-            for nv, sub in enumerate(subs):
-                annots = {
-                    'filename': f'{sub}/{nf:06d}.jpg',
-                    'height': meta['image_shape'][nf][nv][0],
-                    'width': meta['image_shape'][nf][nv][1],
-                    'annots': annots_all[sub],
-                    'isKeyframe': False
-                }
-                dumpname = join(self.output, self.name, sub, '{:06d}.json'.format(nf))
-                save_annot(dumpname, annots)
-
 class WriteSMPL:
     def __init__(self, name='smpl', write_vertices=False) -> None:
         self.name = name
